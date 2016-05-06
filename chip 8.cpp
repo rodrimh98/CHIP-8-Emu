6ddfea6f1153dd46b5cafb0fb4e2d49de78a20fd
rodrimh98 <frontera121@gmail.com>
2016-05-06 16:17:21 +0100

    Added sdl window support

--- chip 8.cpp
+++ chip 8.cpp
@@ -3,6 +3,8 @@
 #include <cstdio>
 #include <stdlib.h>
 #include <string.h>
+#include <SDL2/SDL.h>
+
 using namespace std;
 
 #define MEMSIZE 4096
@@ -55,9 +57,9 @@ void stepmachine (struct machine_t* cpu){
 		uint16_t opcode;
 		opcode = cpu->mem[cpu->pc] << 8 | cpu->mem[cpu->pc + 0x1];
 
-		cout<<hex<<opcode<<endl;
+		cout<<hex<<opcode;
 		cpu->pc = (cpu->pc + 0x2)&0xfff;
-		cout<<cpu->pc<<endl;
+		//cout<<cpu->pc<<endl;
 		uint16_t nnn = opcode & 0x0FFF;
 		uint8_t kk = opcode & 0xFF;
 		uint8_t n = opcode & 0xF;
@@ -84,87 +86,95 @@ void stepmachine (struct machine_t* cpu){
 			break;
 
 			case 2:
-				cout<<"CALL "<<hex<<nnn<<endl;
+				cout<<"CALL "<<nnn<<endl;
 			break;
 
 			case 3: 
 				if (cpu->v[x]==kk)
 					cpu->pc = (cpu->pc +2) & 0xfff;
-				cout<<"SE "<<hex<< x <<" "<<hex<< kk <<endl;
+
+				cout<<"SE "<<x<<" "<<kk<<endl;
+
 			break;
 
 			case 4:
 				if (cpu->v[x] != kk)
 					cpu->pc = (cpu->pc +2 ) & 0xfff;
-				cout<<"SNE "<<hex<<x<<" " <<hex<<kk<<endl; 
+
+				cout<<"SNE "<<x<<" "<<kk<<endl;
+
 			break;
 
 			case 5:
 				if (cpu->v[x] == cpu->v[y])
 					cpu->pc= (cpu->pc +2 ) & 0xfff;
-				cout<<"SE"<<hex<<x<<" "<<hex<<y<<endl;
+
+				cout<<"SE "<<x<<" "<<y<<endl;
 			break;
 
 			case 6:
 				cpu->v[x] = kk; 
-				cout<<"LD "<<hex<< x <<" "<<kk<<endl;
+
+				cout<<"LD "<<x<<" "<<kk<<endl;
 			break;
 
 			case 7:
 				cpu->v[x] += kk;
-				cout<<"ADD "<<hex<< x << " "<<hex<< kk<<endl;
+
+				cout<<"ADD "<<x<<" "<<kk<<endl;
 			break;
 
 			case 8:
 				switch (n){
 					case 0:
 						cpu->v[x] = cpu->v[y];
-						cout<<"LD "<<hex<<x<<hex<<y<<endl;
+						cout<<"LD "<<x<<" "<<y<<endl;
 					break;
 
 					case 1:
 						cpu->v[x] |= cpu->v[y];
-						cout<<"OR "<<hex<<x<<" "<<hex<<y<<endl;
+						cout<<"OR "<<x<<" "<<y<<endl;
 					break;
 
 					case 2:
 						cpu->v[x] &= cpu->v[y];
-						cout<<"AND "<<hex<<x<<" "<<hex<<y<<endl;
+						cout<<"AND "<<x<<" "<<y<<endl;
 					break;
 
 					case 3:
 						cpu->v[x] ^= cpu->v[y];
-						cout<<"XOR "<<hex<<x<<" "<<hex<<y<<endl;
+						cout<<"XOR "<<x<<" "<<y<<endl;
 					break;
 
 					case 4:
 						cpu->v[0xf]=(cpu->v[x] > (cpu->v[x]+cpu->v[y]));
 						cpu->v[x] += cpu->v[y];
-						cout<<"ADD "<<hex<<x<<" "<<hex<<y<<endl;
+						cout<<"ADD "<<x<<" "<<y<<endl;
+
 					break;
 
 					case 5:
 						cpu->v[0xf]= (cpu->v[x] > cpu->v[y]);
 						cpu->v[x] -= cpu->v[y];
-						cout<<"SUB "<<hex<<x<<" "<<hex<<y<<endl;
+						cout<<"SUB "<<x<<" "<<y<<endl;
 					break;
 
 					case 6:
 						cpu->v[0xf] = cpu->v[x] & 0x01;
 						cpu->v[x] = cpu->v[x] >> 1; 
- 						cout<<"SHR "/*<<hex*/<<x<< " "/*<<hex*/<<y<<endl;
+						cout<<"SHR "<<x<<endl;
 					break;
 
 					case 7:
 						cpu->v[0xf] = (cpu->v[y] > cpu->v[x]);
 						cpu->v[x]= cpu->v[y]-cpu->v[x];
-						cout<<"SUBN "<<hex<<x<<" "<<hex<<y<<endl;
+						cout<<"SUBN "<<x<<" "<<y<<endl;
 					break;
 
 					case 0xE:
 						cpu->v[0xf] = ((cpu->v[x] & 0x80) != 0);
 						cpu->v[x] = cpu->v[x] << 1;
-						cout<<"SHL "<<hex<<x<<" "<<hex<<y<<endl;
+						cout<<"SHL "<<x<<endl;
 					break;
 
 				}
@@ -174,26 +184,26 @@ void stepmachine (struct machine_t* cpu){
 			case 9:
 				if (cpu->v[x] != cpu->v[y]);
 					cpu->pc = (cpu->pc + 2) & 0xfff;
-				cout<<"SNE "<<hex<<x<<" "<<hex<<y<<endl;
+
+				cout<<"SNE "<<x<<" "<<y<<endl;
 			break;
 
 			case 0xA:
 				cpu->i = nnn;
-				cout<<"LD I"<<" "<<hex<< nnn <<endl;
+				cout<<"LD I "<<nnn<<endl;
 			break;
 
 			case 0xB:
 				cpu->pc = cpu->v[0] + nnn;
-				cout<<"JP V0 "<< nnn << endl;
+				cout<<"JP "<<cpu->v[0]<<" "<<nnn<<endl;
 			break;
 
 			case 0xC:
-
-				cout<<"RND "<<hex<< x << " " << kk<<endl;
+				cout<<"RND "<< x << " " << kk<<endl;
 			break;
 
 			case 0xD:
-				cout<<"DRW "<<hex<<x<<" "<<hex<<y<<" "<<n<<endl;
+				cout<<"DRW "<<x<<" "<<y<<" "<<n<<endl;
 			break;
 			
 			case 0xE:
@@ -212,44 +222,44 @@ void stepmachine (struct machine_t* cpu){
 				switch (kk){
 					case 0x07:
 						cpu->v[x] = cpu->dt;
-						cout<<"LD "<<hex<<x <<" dt" << endl;
+						cout<<"LD "<<x<<" dt"<<endl;
 					break;
 
 					case 0x0A:
 						//fixme
-						cout<<"LD "<<hex<<x<<endl;
+						cout<<"LD "<<x<<endl;
 					break;
 
 					case 0x15:
 						cpu->dt = cpu->v[x];
-						cout<<"LD "<<"dt "<<hex<<x<<endl;
+						cout<<"LD dt "<<x<<endl;
 					break;
 
 					case 0x18:
 						cpu->dt= cpu->v[x];
-						cout<<"LD "<<"st "<<hex<<x<<endl;
+						cout<<"LD st"<<x<<endl;
 					break;
 
 					case 0x1E:
 						cpu->i += cpu->v[x];
-						cout <<"ADD "<< "i " <<hex<< x<<endl;
+						cout<<"ADD I "<<x<<endl;
 					break;
 
 					case 0x29:
-						cout<<"LD F "<<hex<< x << endl;
+						cout<<"LD F "<< x << endl;
 					break;
 
 					case 0x33:
 
-						cout<<"LD B "<<hex<< x<<endl;
+						cout<<"LD B "<< x<<endl;
 					break;
 
 					case 0x55:
-						cout<<"LD I "<<hex<<x<<endl;
+						cout<<"LD I "<<x<<endl;
 					break;
 
 					case 0x65:
-						cout<<"LD "<<hex<<x<<" I"<<endl;
+						cout<<"LD "<<x<<" I"<<endl;
 					break;
 				}
 			break;
@@ -258,14 +268,58 @@ void stepmachine (struct machine_t* cpu){
 
 int main (int argc, const char * argv[]){
 
-		struct machine_t mac;
-		init_machine(&mac);
-		load_rom(&mac);
-		stepmachine(&mac);
-		cout<<ascii(mac.pc)<<endl;
-		//while(mac.pc<0xfff){
-		//stepmachine(&mac);
-		//}
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Texture* texture;
+    SDL_Surface* surface;
+    SDL_Event event;
+    struct machine_t mac;
+    int mustQuit = 0;
+ 
+    // Init emulator
+    init_machine(&mac);
+    load_rom(&mac);
+    
+    // Init SDL engine
+    SDL_Init(SDL_INIT_EVERYTHING);
+    window = SDL_CreateWindow("CHIP-8 Emulator",
+                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              640, 320, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
+                                SDL_TEXTUREACCESS_STREAMING, 64, 32);
+    
+    // Write some dummy white background
+    surface = SDL_CreateRGBSurfaceFrom(NULL, 64, 32, 32, 0,
+                                       0x00FF0000,
+                                       0x0000FF00,
+                                       0x000000FF,
+                                       0xFF000000);
+    SDL_LockTexture(texture, NULL, &surface->pixels, &surface->pitch);
+    memset(surface->pixels, 0xFF, surface->pitch * 32);
+    SDL_UnlockTexture(texture);
+
+    while (!mustQuit) {
+        // step_machine(&mac);
+        // Disabled. You'll see at the next live coding session why.
+
+        SDL_WaitEvent(&event);
+        switch (event.type) {
+            case SDL_QUIT:
+                mustQuit = 1;
+                break;
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+    
+    // Dispose SDL engine.
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 	return 0;
 
 }
